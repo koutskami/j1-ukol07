ec4a967eba716dc5140881e96b98302fe0820207
koutskami <koutskami@gmail.com>
2024-03-27 21:49:13 +0100

    vypracovani tridy Kniha

--- src/main/java/cz/czechitas/ukol07/Kniha.java
+++ src/main/java/cz/czechitas/ukol07/Kniha.java
@@ -1,5 +1,32 @@
 package cz.czechitas.ukol07;
 
 public class Kniha {
-    //autor string, nazev string, rokVydani int
+    //autor string, nazev string, rokVydani int - struktura podle jsonu
+    private String autorKnihy;
+    private String nazevKnihy;
+    private Integer rokVydaniKnihy;
+
+    //getter a setter pro autora knihy
+    public String getAutorKnihy() {
+        return autorKnihy;
+    }
+    public void setAutorKnihy(String autorKnihy) {
+        this.autorKnihy = autorKnihy;
+    }
+
+    //getter a setter pro nazev knihy
+    public String getNazevKnihy() {
+        return nazevKnihy;
+    }
+    public void setNazevKnihy(String nazevKnihy) {
+        this.nazevKnihy = nazevKnihy;
+    }
+
+    //getter a setter pro rok vydani knihy
+    public Integer getRokVydaniKnihy() {
+        return rokVydaniKnihy;
+    }
+    public void setRokVydaniKnihy(Integer rokVydaniKnihy) {
+        this.rokVydaniKnihy = rokVydaniKnihy;
+    }
 }
