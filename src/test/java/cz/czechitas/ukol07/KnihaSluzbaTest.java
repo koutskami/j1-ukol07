e62d4177aafaa5dd0357522365e5b9eb1039debb
koutskami <koutskami@gmail.com>
2024-03-31 22:35:58 +0200

    procisteni hotovych TODO

--- src/test/java/cz/czechitas/ukol07/KnihaSluzbaTest.java
+++ src/test/java/cz/czechitas/ukol07/KnihaSluzbaTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class KnihaSluzbaTest {
-//TODO Napište testy pro metody.
-//TODO Nezapomeňte otestovat i okrajové případy – metoda pro výběr knih dle autora nebo orku nevrátí žádnou knížku nebo vrátí více knih.
+//Napište testy pro metody.
+//Nezapomeňte otestovat i okrajové případy – metoda pro výběr knih dle autora nebo orku nevrátí žádnou knížku nebo vrátí více knih.
 
     @Test
     void vypisSeznamVsechKnih() {
